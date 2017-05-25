219b44309901650b7bca1c1208f14fa8eead6db2
karimaelo <k.code@outlook.com>
2017-05-25 13:39:34 -0400

    added leaderboard js files

--- app/controllers/dashboardController.js
+++ app/controllers/dashboardController.js
@@ -5,6 +5,126 @@
     var DashboardController = function ($scope, $http) {
         $scope.greeting = "Hola!!!";
         $scope.users = users;
+
+        function init() {
+            disChart();
+            matChart();
+            disMatStarChart();
+        }
+
+        function disMatStarChart() {
+            var dismatCtx = document.getElementById("dismat").getContext('2d');
+            var dismat = new Chart(dismatCtx, {
+                type: 'radar',
+                data: {
+                    labels: ["DIS", "MAT", "AMZN"],
+                    datasets: [{
+                        label: 'DIS vs MAT',
+                        data: [110, 90, 80],
+                        backgroundColor: [
+                            'rgba(75, 192, 192, 0.2)',
+                            'rgba(255, 99, 132, 0.2)'                            
+                        ],
+                        borderColor: [
+                            'rgba(54, 162, 235, 1)',
+                            'rgba(255, 206, 86, 1)'                            
+                        ],
+                        borderWidth: 1
+                    }]
+                },
+                options: {
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero: true
+                            }
+                        }]
+                    }
+                }
+            });
+        }
+
+        function disChart() {
+            var disCtx = document.getElementById("dis").getContext('2d');
+            var dis = new Chart(disCtx, {
+                type: 'line',
+                data: {
+                    labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
+                    datasets: [{
+                        label: 'DIS',
+                        data: [110, 50, 77, 120, 110, 40],
+                        backgroundColor: [
+                            'rgba(75, 192, 192, 0.2)',
+                            'rgba(255, 99, 132, 0.2)',
+                            'rgba(54, 162, 235, 0.2)',
+                            'rgba(255, 206, 86, 0.2)',
+                            'rgba(75, 192, 192, 0.2)',
+                            'rgba(153, 102, 255, 0.2)'
+                        ],
+                        borderColor: [
+                            'rgba(54, 162, 235, 1)',
+                            'rgba(255, 206, 86, 1)',
+                            'rgba(75, 192, 192, 1)',
+                            'rgba(153, 102, 255, 1)',
+                            'rgba(255, 159, 64, 1)',
+                            'rgba(255,99,132,1)'
+                        ],
+                        borderWidth: 1
+                    }]
+                },
+                options: {
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero: true
+                            }
+                        }]
+                    }
+                }
+            });
+        }
+
+        function matChart() {
+            var matCtx = document.getElementById("mat").getContext('2d');
+            var mat = new Chart(matCtx, {
+                type: 'line',
+                data: {
+                    labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
+                    datasets: [{
+                        label: 'MAT',
+                        data: [90, 50, 120, 40, 20, 110],
+                        backgroundColor: [
+                            'rgba(255, 99, 132, 0.2)',
+                            'rgba(54, 162, 235, 0.2)',
+                            'rgba(255, 206, 86, 0.2)',
+                            'rgba(75, 192, 192, 0.2)',
+                            'rgba(153, 102, 255, 0.2)',
+                            'rgba(255, 159, 64, 0.2)'
+                        ],
+                        borderColor: [
+                            'rgba(255,99,132,1)',
+                            'rgba(54, 162, 235, 1)',
+                            'rgba(255, 206, 86, 1)',
+                            'rgba(75, 192, 192, 1)',
+                            'rgba(153, 102, 255, 1)',
+                            'rgba(255, 159, 64, 1)'
+                        ],
+                        borderWidth: 1
+                    }]
+                },
+                options: {
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero: true
+                            }
+                        }]
+                    }
+                }
+            });
+        }
+
+        init();
     };
 
     
